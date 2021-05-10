5daef4784ad64ec5b486e98c8efa48de93179c29
nikollanik17 <nikollanik17@gmail.com>
2021-05-10 23:46:36 +0200

    add basic UI with navbar, swiper, concept

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "styles/globals.scss";
+import "styles/navbarreset.scss";
+
+// swiper
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
+import "rc-slider/assets/index.css";
+
+// components
+import Navbar from "components/Layout/Navbar/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
